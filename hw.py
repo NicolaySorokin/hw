97a4e8a6b9676404324da649ab50397748e20221
Nicolay Sorokin <sorokin.n.04@mail.ru>
2025-04-19 14:16:20 +0300

    Add hw

--- hw.py
+++ hw.py
@@ -1,8 +1,22 @@
-import string
+import random
+from math import gcd
 
 
-def caesar_cipher(text, shift):
-    alphabet = string.ascii_lowercase
-    shifted_alphabet = alphabet[shift % 26:] + alphabet[:shift % 26]
-    table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
-    return text.translate(table)
+def generate_keys():
+    primes = [i for i in range(2, 10**6) if all(i % d != 0 for d in range(2, int(i**0.5) + 1))]
+    p, q = random.sample(primes, 2)
+    n = p * q
+    phi_n = (p - 1) * (q - 1)
+    e = next(x for x in range(2, phi_n) if gcd(x, phi_n) == 1)
+    d = pow(e, -1, phi_n)
+    return (e, n), (d, n)
+
+
+def encrypt(message, public_key):
+    e, n = public_key
+    return [pow(ord(char), e, n) for char in message]
+
+
+def decrypt(ciphertext, private_key):
+    d, n = private_key
+    return ''.join(chr(pow(char, d, n)) for char in ciphertext)
--- test_hw.py
+++ test_hw.py
@@ -1,15 +1,31 @@
 import unittest
-from hw import caesar_cipher
+from hw import generate_keys, encrypt, decrypt
 
 
-class TestCaesar(unittest.TestCase):
-    def test_encryption(self):
-        self.assertEqual(caesar_cipher("hello", 3), "khoor")
-        self.assertEqual(caesar_cipher("abc", 1), "bcd")
-        self.assertEqual(caesar_cipher("xyz", 2), "zab")
+class TestRSA(unittest.TestCase):
+    def setUp(self):
+        self.public_key, self.private_key = generate_keys()
+        self.message = "Hello"
 
-    def test_case_sensitivity(self):
-        self.assertEqual(caesar_cipher("Hello, World!", 5), "Mjqqt, Btwqi!")
+    def test_key_generation(self):
+        self.assertNotEqual(self.public_key, self.private_key)
 
-    def test_large_shifts(self):
-        self.assertEqual(caesar_cipher("abcdz", 27), "bcdea")
+    def test_encryption_decryption(self):
+        ciphertext = encrypt(self.message, self.public_key)
+        decrypted_message = decrypt(ciphertext, self.private_key)
+        self.assertEqual(self.message, decrypted_message)
+
+    def test_encryption_output(self):
+        ciphertext = encrypt(self.message, self.public_key)
+        self.assertIsInstance(ciphertext, list)
+        self.assertGreater(len(ciphertext), 0)
+
+    def test_decryption_output(self):
+        ciphertext = encrypt(self.message, self.public_key)
+        decrypted_message = decrypt(ciphertext, self.private_key)
+        self.assertIsInstance(decrypted_message, str)
+
+    def test_message_integrity(self):
+        ciphertext = encrypt(self.message, self.public_key)
+        decrypted_message = decrypt(ciphertext, self.private_key)
+        self.assertEqual(self.message, decrypted_message)
